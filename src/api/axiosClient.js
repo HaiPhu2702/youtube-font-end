109448a780ce6326098a981fc90cefe251269ed9
HaiPhu2702 <102145150+HaiPhu2702@users.noreply.github.com>
2023-02-20 16:34:07 +0700

    dsfdsf

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -5,7 +5,8 @@ const axiosClient = axios.create({
   baseURL: "https://youtube-back-end.vercel.app/api",
   headers: {
     'content-type': 'application/json',
-    'access-control-origin': '*'
+    'access-control-origin': '*',
+    'X-Requested-With': 'XMLHttpRequest'
 },
   paramsSerializer: (params) => queryString.stringify(params),
 });

086391d50886cd97205e491126d1a0b11020782c
HaiPhu2702 <102145150+HaiPhu2702@users.noreply.github.com>
2023-02-28 18:46:53 +0700

    fdsfdsf

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import queryString from 'query-string';
 
 const axiosClient = axios.create({
-  baseURL: "https://back-youtubee.vercel.app/api",
+  baseURL: "https://backend-youtube-1l61.onrender.com/api",
   headers: {
     'content-type': 'application/json'
 },
